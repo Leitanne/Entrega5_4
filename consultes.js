2e67ac827d88f9bdef580fbafd152235f74e5b7b
Leitanne <gina.rico.llorca@gmail.com>
2023-10-02 20:42:13 +0200

    Se han registrado unas cuantas queries mas

--- consultes.js
+++ consultes.js
@@ -35,7 +35,30 @@ async function consulta() {
         //resultats = restaurants.find({grades: {$elemMatch: {score: {$gt: 90}}}});
 
         // 9. Escriu una consulta per trobar els restaurants que tenen un score més gran que 80 però menys que 100.
-        resultats = restaurants.find({grades: {$elemMatch: {score: {$in: [80, 100]}}}})
+        //resultats = restaurants.find({grades: {$elemMatch: {score: {$in: [80, 100]}}}});
+        
+        // 10. Escriu una consulta per trobar els restaurants que estan situats en una longitud inferior a -95.754168.
+        //resultats = restaurants.find({'address.coord.0': {$lt: -95.754168}});
+
+        // 11. Escriu una consulta de MongoDB per a trobar els restaurants que no cuinen menjar 'American ' i tenen algun score superior a 70 i longitud inferior a -65.754168.
+        //resultats = restaurants.find({$and:[{cuisine: {$ne: 'American'}},{grades: {$elemMatch: {score: {$gt: 70}}}},{'address.coord.0': {$lt: -65.754168}}]});
+        
+        // 12. Escriu una consulta per trobar els restaurants que no preparen menjar 'American' i tenen algun score superior a 70 i que, a més, es localitzen en longituds inferiors a -65.754168. Nota: Fes aquesta consulta sense utilitzar operador $and.
+        //resultats = restaurants.find({cuisine: {$ne: 'American'}, grades: {$elemMatch: {score: {$gt: 70}}}, 'address.coord.0': {$lt: -65.754168}});
+
+        // 13. Escriu una consulta per trobar els restaurants que no preparen menjar 'American ', tenen alguna nota 'A' i no pertanyen a Brooklyn. S'ha de mostrar el document segons la cuisine en ordre descendent.
+        //resultats = restaurants.find({cuisine: {$ne: 'American'}, grades: {$elemMatch: {grade: "A"}}, borough: {$ne: "Brooklyn"}}).sort({cuisine: -1});
+
+        // 14. Escriu una consulta per trobar el restaurant_id, name, borough i cuisine per a aquells restaurants que contenen 'Wil' en les tres primeres lletres en el seu nom.
+        //resultats = restaurants.find({name: /^Wil/}).project({restaurant_id: 1, name: 1, borough: 1, cuisine: 1});
+        
+        // 15. Escriu una consulta per trobar el restaurant_id, name, borough i cuisine per a aquells restaurants que contenen 'ces' en les últimes tres lletres en el seu nom.
+        // resultats = restaurants.find({name: /ces$/}).project({restaurant_id: 1, name: 1, borough: 1, cuisine: 1});
+        
+        // 16. Escriu una consulta per trobar el restaurant_id, name, borough i cuisine per a aquells restaurants que contenen 'Reg' en qualsevol lloc del seu nom.
+        //resultats = restaurants.find({name: /Reg/g}).project({restaurant_id: 1, name: 1, borough: 1, cuisine: 1});
+
+        // 17. Escriu una consulta per trobar els restaurants que pertanyen al Bronx i preparen plats Americans o xinesos.
         
         for await (const resultat of resultats) {
             console.dir(resultat);
